de2fef0fe3e71dc4e7eb8fbc12105557b2f6ab25
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2020-04-19 09:13:27 +0900

    Remove CMake files and unused translation files

--- data/update-icons.sh
+++
@@ -1,7 +0,0 @@
-cp hourglass.svg icons/16x16/apps/
-cp hourglass.svg icons/24x24/apps/
-cp hourglass.svg icons/32x32/apps/
-cp hourglass.svg icons/48x48/apps/
-cp hourglass.svg icons/64x64/apps/
-cp hourglass.svg icons/128x128/apps/
-cp hourglass.svg icons/scalable/apps/
